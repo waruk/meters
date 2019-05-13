07672e7f17fc7628370063d36af9105840717e28
Andrei Ciurea <aciurea@outlook.com>
2019-05-13 15:50:05 +0300

    Added some html examples.

---
+++ Meters.Web/scripts/main.js
@@ -0,0 +1,10 @@
+﻿window.onload = function () {
+    var delButtons = document.getElementsByClassName("delete-button");
+    for (var i = 0; i < delButtons.length; i++) {
+        delButtons[i].addEventListener("click", DeleteConfirm, false);
+    }
+}
+
+function DeleteConfirm() {
+    alert("tets");
+};

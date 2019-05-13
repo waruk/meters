a9e2487ba7d2a5c91b2d0d083671a8d390c9f114
Andrei Ciurea <aciurea@outlook.com>
2019-05-13 17:51:05 +0300

    Added some validations on entities.

--- Meters.Data/Entities/Index.cs
+++ Meters.Data/Entities/Index.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Meters.Data.Entities
 {
     public class Index
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IndexId { get; set; }
+
+        [Required]
         public DateTime RegisteredDate { get; set; }
+
+        [Required]
         public double Value { get; set; }
 
         public int MeterId { get; set; }
+
+        [ForeignKey("MeterId")]
         public Meter Meter { get; set; }      // reference to the meter this index is linked to
     }
 }
--- Meters.Data/Entities/Meter.cs
+++ Meters.Data/Entities/Meter.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Meters.Data.Entities
 {
     public class Meter
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int MeterId { get; set; }
+
+        [MaxLength(50)]
         public string SerialNumber { get; set; }
+
+        [Required]
         public MeteredResource ResourceType { get; set; }
+
+        [Required]
         public MeterLocation Location { get; set; }
+
         public ICollection<Index> Indexes { get; set; } = new List<Index>();    // good idea to initialize it to prevent null reference exceptions
     }
 }
